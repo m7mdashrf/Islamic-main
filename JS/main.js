96ca6f96ebb1a2b11a8bf12d867d70def3100519
m7mdashrf <m7mdashrf01@gmail.com>
2022-04-13 18:02:56 +0200

    commit

--- JS/main.js
+++ JS/main.js
@@ -85,7 +85,7 @@ let surahsContainer = document.querySelector('.surahsContainer');
 
 function getSurahs() {
     // Fetch Surahs Data {Name Of Surahs}
-    fetch(' https://api.alquran.cloud/v1/meta')
+    fetch('https://api.alquran.cloud/v1/meta')
         .then(response => response.json())
         .then(data => {
             let surahs = data.data.surahs.references;
